97d828de3fb9ced2cf09bdb7a4e171187f2371c7
Hennan Lewis <39320310+hennanlewis@users.noreply.github.com>
2024-12-02 02:41:31 -0300

    feat: organiza `printer_manager` em módulo

---
+++ printer_manager/printer_manager/printer_manager.py
@@ -0,0 +1,63 @@
+import win32print
+
+class PrinterManager:
+
+    @staticmethod
+    def get_printer_list():
+        """Retorna a lista de impressoras atuais do computador com seus respectivos monitores."""
+        printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL | win32print.PRINTER_ENUM_CONNECTIONS)
+        printer_info = []
+        for printer in printers:
+            printer_name = printer[2]  # Nome da impressora
+            # Obtém o monitor associado à impressora
+            handle = win32print.OpenPrinter(printer_name)
+            printer_details = win32print.GetPrinter(handle, 2)  # Obtém informações detalhadas
+
+            # Removendo os campos 'pDevMode' (PyDEVMODEW) e 'pSecurityDescriptor' (PySECURITY_DESCRIPTOR)
+            printer_details.pop('pDevMode', None)
+            printer_details.pop('pSecurityDescriptor', None)
+
+            # Adiciona um dicionário simplificado com dados serializáveis
+            printer_info.append(printer_details)
+
+        return printer_info
+
+    @staticmethod
+    def get_ports(printer_name):
+        """Recebe o nome da impressora e retorna o nome das portas da impressora recebida."""
+        try:
+            printer_info = win32print.GetPrinter(printer_name, 2)
+            ports = printer_info['pPort']
+            return ports
+        except Exception as e:
+            return f"Erro ao obter portas: {e}"
+
+    @staticmethod
+    def check_port_exists(printer_name, port_name):
+        """Confere se uma porta de uma impressora X existe."""
+        ports = PrinterManager.get_ports(printer_name)
+        return port_name in ports if isinstance(ports, list) else False
+
+    @staticmethod
+    def set_port_value(printer_name, port_name, value, monitor=None):
+        """Altera o valor da porta da impressora."""
+        try:
+            return f"Alterando porta: Impressora: {printer_name}, Porta: {port_name}, Valor: {value}, Monitor: {monitor}"
+        except Exception as e:
+            return f"Erro ao alterar porta: {e}"
+
+    @staticmethod
+    def create_port(printer_name, port_name, value, monitor=None):
+        """Cria uma nova porta com o valor especificado para a impressora."""
+        try:
+            return f"Criando porta: Impressora: {printer_name}, Porta: {port_name}, Valor: {value}, Monitor: {monitor}"
+        except Exception as e:
+            return f"Erro ao criar porta: {e}"
+
+    @staticmethod
+    def delete_port(printer_name, port_name):
+        """Exclui uma porta de uma impressora se existir."""
+        try:
+            return f"Excluindo porta: Impressora: {printer_name}, Porta: {port_name}"
+        except Exception as e:
+            return f"Erro ao excluir porta: {e}"
